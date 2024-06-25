63534d8bff719e96b8f4c3c3f377d6d3ddcba86e
MatejKurevija <matej375@gmail.com>
2024-06-25 18:19:57 +0200

    Switch strane boxa kad napada
    Zamjena strana napadanja i smanjeni hitboxovi da odgovoaraju animacijama

--- Assets/SwordAttack.cs
+++ Assets/SwordAttack.cs
@@ -7,6 +7,12 @@ public class SwordAttack : MonoBehaviour
     public GameObject swordCollider;
     public float attackCooldown = 0.5f;
     private float lastAttackTime = 0;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
@@ -19,6 +25,7 @@ public class SwordAttack : MonoBehaviour
     void Attack()
     {
         lastAttackTime = Time.time;
+        AdjustSwordCollider();
         swordCollider.SetActive(true);
         Invoke("DeactivateSwordCollider", 0.1f);
     }
@@ -26,5 +33,25 @@ public class SwordAttack : MonoBehaviour
     void DeactivateSwordCollider()
     {
         swordCollider.SetActive(false);
+    }
+
+    void AdjustSwordCollider()
+    {
+        Vector3 newPosition = swordCollider.transform.localPosition;
+        Vector3 newScale = swordCollider.transform.localScale;
+
+        if (spriteRenderer.flipX)
+        {
+            newPosition.x = -Mathf.Abs(newPosition.x); // Inverzija x pozicije
+            newScale.x = -Mathf.Abs(newScale.x); // Inverzija x skale
+        }
+        else
+        {
+            newPosition.x = Mathf.Abs(newPosition.x); // Postavi na normalnu x poziciju
+            newScale.x = Mathf.Abs(newScale.x); // Postavi na normalnu x skalu
+        }
+
+        swordCollider.transform.localPosition = newPosition;
+        swordCollider.transform.localScale = newScale;
     }
 }
