11a29ed34d79dc7d9c5901252b10e000d02414d1
vidakovicrobert <vidakovic.robert19@gmail.com>
2024-08-31 19:49:30 +0200

    Dodan enemy damage
    Neprijatelj ozljeđuje igrača dok se nalazi u njegovom hitbox-u - modificirana MonsterMovement skripta

--- Assets/MonsterMovement.cs
+++ Assets/MonsterMovement.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class MonsterMovement : MonoBehaviour, IDamageable
 {
     public Transform[] patrolPoints;
     public float moveSpeed;
     public int patrolTarget;
     public int health = 6;
+    public int damageAmount = 1;  // Damage dealt per interval
+    public float damageInterval = 1f;  // Time between damage ticks
+
+    private float lastDamageTime;
 
     void Start()
     {
         patrolTarget = 0;
+        lastDamageTime = -damageInterval;  // Ensures immediate damage on first contact
     }
 
     void Update()
@@ -48,5 +52,36 @@ public class MonsterMovement : MonoBehaviour, IDamageable
     {
         Debug.Log(gameObject.name + " is dead.");
         Destroy(gameObject);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            // Deal damage at regular intervals
+            if (Time.time >= lastDamageTime + damageInterval)
+            {
+                var healthComponent = collision.GetComponent<PlayerHealth>();
+                if (healthComponent != null)
+                {
+                    healthComponent.TakeDamage(damageAmount);
+                    lastDamageTime = Time.time;  // Reset damage timer
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Optionally, deal immediate damage upon first contact
+        if (collision.tag == "Player")
+        {
+            var healthComponent = collision.GetComponent<PlayerHealth>();
+            if (healthComponent != null)
+            {
+                healthComponent.TakeDamage(damageAmount);
+                lastDamageTime = Time.time;  // Reset damage timer
+            }
+        }
     }
 }
