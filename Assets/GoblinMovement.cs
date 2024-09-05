2756c1540ffaf2a46a37c967cef89e964ec25ec2
Antonio Pavlović <anpavlovic@student.unipu.hr>
2024-09-05 20:07:12 +0200

    Monster Changes

--- Assets/GoblinMovement.cs
+++ Assets/GoblinMovement.cs
@@ -9,17 +9,21 @@ public class GoblinMovement : MonoBehaviour, IDamageable
     public float attackRange = 1f;
     public Transform player;
     public int health = 6;
+    public int damageAmount = 1;  // Koliko štete goblin nanosi igraču
+    public float damageInterval = 1f;  // Koliko često može nanijeti štetu igraču
 
     private float stepCounter;
     private int direction = 1;
     private float nextFlipTime;
     private float flipInterval = 4f;
     private bool isAttacking = false;
+    private float lastDamageTime;  // Kada je goblin zadnji put nanio štetu
 
     void Start()
     {
         stepCounter = stepLength;
         nextFlipTime = Time.time + flipInterval;
+        lastDamageTime = -damageInterval;  // Omogućuje trenutni napad na početku
     }
 
     void Update()
@@ -60,16 +64,29 @@ public class GoblinMovement : MonoBehaviour, IDamageable
 
     private void AttackPlayer()
     {
-        Vector3 directionToPlayer = (player.position - transform.position).normalized;
-        transform.Translate(directionToPlayer * speed * Time.deltaTime);
+        // Provjera ako može nanijeti štetu
+        if (Time.time >= lastDamageTime + damageInterval)
+        {
+            Vector3 directionToPlayer = (player.position - transform.position).normalized;
+            transform.Translate(directionToPlayer * speed * Time.deltaTime);
 
-        if (player.position.x > transform.position.x && direction != 1)
-        {
-            Flip();
-        }
-        else if (player.position.x < transform.position.x && direction != -1)
-        {
-            Flip();
+            // Promjena smjera ovisno o poziciji igrača
+            if (player.position.x > transform.position.x && direction != 1)
+            {
+                Flip();
+            }
+            else if (player.position.x < transform.position.x && direction != -1)
+            {
+                Flip();
+            }
+
+            // Pronađi komponentu zdravlja igrača i nanesi štetu
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damageAmount);
+                lastDamageTime = Time.time;  // Postavi zadnji napad na sada
+            }
         }
     }
 
@@ -95,5 +112,22 @@ public class GoblinMovement : MonoBehaviour, IDamageable
         Vector2 scaler = transform.localScale;
         scaler.x *= -1;
         transform.localScale = scaler;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        // Ako je igrač unutar napada, nanesi mu štetu
+        if (collision.CompareTag("Player"))
+        {
+            if (Time.time >= lastDamageTime + damageInterval)
+            {
+                var healthComponent = collision.GetComponent<PlayerHealth>();
+                if (healthComponent != null)
+                {
+                    healthComponent.TakeDamage(damageAmount);
+                    lastDamageTime = Time.time;
+                }
+            }
+        }
     }
 }
